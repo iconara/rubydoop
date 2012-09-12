63d929ebb4341cbbb29dcf9b94a9a49ab63029d8
Theo <theo@iconara.net>
2012-09-12 11:41:43 +0200

    Updated the word count example

--- examples/word_count/lib/word_count.rb
+++ examples/word_count/lib/word_count.rb
@@ -1,15 +1,14 @@
 # encoding: utf-8
 
 require 'rubydoop/configure'
-require 'json'
 require 'word_count/mapper'
 require 'word_count/reducer'
 
 
 configure do |input_path, output_path|
   job 'word_count' do
-    input input_path, :format => Hadoop::Mapred::TextInputFormat
-    output output_path, :format => Hadoop::Mapred::TextOutputFormat
+    input input_path
+    output output_path
 
     mapper WordCount::Mapper
     reducer WordCount::Reducer
--- examples/word_count/lib/word_count/reducer.rb
+++ examples/word_count/lib/word_count/reducer.rb
@@ -2,14 +2,11 @@
 
 module WordCount
   class Reducer
-    def configure(conf)
-    end
-
-    def reduce(key, values, output, reporter)
+    def reduce(key, values, context)
       total_sum = values.reduce(0) do |sum, value|
         sum + value.get
       end
-      output.collect(key, Hadoop::Io::IntWritable.new(total_sum));
+      context.write(key, Hadoop::Io::IntWritable.new(total_sum));
     end
   end
 end
