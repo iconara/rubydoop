3bfd5a2a030b6429df822f85dabcc6f5e44a8470
Gustav Munkby <grddev@gmail.com>
2012-12-06 14:51:43 +0100

    Rework JobDefinition spec to use real Job/Configuration

--- spec/rubydoop/job_definition_spec.rb
+++ spec/rubydoop/job_definition_spec.rb
@@ -4,13 +4,18 @@ require_relative '../spec_helper'
 
 
 module Rubydoop
+  java_import 'org.apache.hadoop.conf.Configuration'
+
   describe JobDefinition do
-    let :configuration do
-      stub(:configuration)
+    let :job do
+      config = Configuration.new
+      config.set 'mapred.job.tracker', 'local'
+      config.set 'fs.default.name', 'file:///'
+      Hadoop::Mapreduce::Job.new(config)
     end
 
-    let :job do
-      stub(:job, :configuration => configuration)
+    let :configuration do
+      job.configuration
     end
 
     let :context do
@@ -23,23 +28,23 @@ module Rubydoop
 
     describe '#set' do
       it 'sets a string property on the job\'s configuration' do
-        configuration.should_receive(:set).with('apa', 'bepa')
         job_definition.set('apa', 'bepa')
+        configuration.get('apa').should == 'bepa'
       end
 
       it 'sets a long integer property on the job\'s configuration when given a fixnum' do
-        configuration.should_receive(:set_long).with('apa', 42)
         job_definition.set('apa', 42)
+        configuration.get_long('apa', 0).should == 42
       end
 
       it 'sets a boolean property on the job\'s configuration when given a bool' do
-        configuration.should_receive(:set_boolean).with('apa', true)
         job_definition.set('apa', true)
+        configuration.get_boolean('apa', false).should be_true
       end
 
       it 'sets a float property on the job\'s configuration when given a float' do
-        configuration.should_receive(:set_float).with('apa', 3.14)
         job_definition.set('apa', 3.14)
+        configuration.get_float('apa', 0.0).should be_within(0.001).of(3.14)
       end
     end
   end
