4afa281abece3bee79d5b574ad55528533f4a69b
Gustav Munkby <grddev@gmail.com>
2014-08-12 11:08:51 +0200

    RSpec future-proof existing specs

--- spec/integration/integration_spec.rb
+++ spec/integration/integration_spec.rb
@@ -44,61 +44,61 @@ describe 'Packaging and running a project' do
     end
 
     it 'includes the project files' do
-      jar_entries.should include('test_project.rb')
-      jar_entries.should include('word_count.rb')
-      jar_entries.should include('uniques.rb')
+      expect(jar_entries).to include('test_project.rb')
+      expect(jar_entries).to include('word_count.rb')
+      expect(jar_entries).to include('uniques.rb')
     end
 
     it 'includes gem dependencies' do
-      jar_entries.grep(%r'^gems/paint-[^/]+/lib').should_not be_empty
+      expect(jar_entries.grep(%r'^gems/paint-[^/]+/lib')).not_to be_empty
     end
 
     if JRUBY_VERSION =~ /^1\.(?:6|7\.[0-4]$)/
       it 'includes gems that are built into future jruby releases' do
-        jar_entries.grep(%r'^gems/json-[^/]+/lib').should_not be_empty
-        jar_entries.grep(%r'^gems/jruby-openssl-[^/]+/lib').should_not be_empty
+        expect(jar_entries.grep(%r'^gems/json-[^/]+/lib')).not_to be_empty
+        expect(jar_entries.grep(%r'^gems/jruby-openssl-[^/]+/lib')).not_to be_empty
       end
     else
       it 'ignores default gems' do
-        jar_entries.grep(%r'^gems/json-[^/]+/lib').should be_empty
-        jar_entries.grep(%r'^gems/jruby-openssl-[^/]+/lib').should be_empty
+        expect(jar_entries.grep(%r'^gems/json-[^/]+/lib')).to be_empty
+        expect(jar_entries.grep(%r'^gems/jruby-openssl-[^/]+/lib')).to be_empty
       end
     end
 
     it 'includes the Rubydoop gem' do
-      jar_entries.should include("gems/rubydoop-#{Rubydoop::VERSION}/lib/rubydoop.rb")
-      jar_entries.should include("gems/rubydoop-#{Rubydoop::VERSION}/lib/rubydoop/dsl.rb")
+      expect(jar_entries).to include("gems/rubydoop-#{Rubydoop::VERSION}/lib/rubydoop.rb")
+      expect(jar_entries).to include("gems/rubydoop-#{Rubydoop::VERSION}/lib/rubydoop/dsl.rb")
     end
 
     it 'includes a script that sets up a load path that includes all bundled gems' do
       file_io = jar.get_input_stream(jar.get_jar_entry('setup_load_path.rb')).to_io
       script_contents = file_io.read
-      script_contents.should include(%($LOAD_PATH << 'gems/rubydoop-#{Rubydoop::VERSION}/lib'))
-      script_contents.should match(%r"'gems/paint-[^/]+/lib'")
+      expect(script_contents).to include(%($LOAD_PATH << 'gems/rubydoop-#{Rubydoop::VERSION}/lib'))
+      expect(script_contents).to match(%r"'gems/paint-[^/]+/lib'")
       if JRUBY_VERSION =~ /^1\.(?:6|7\.[0-4]$)/
-        script_contents.should match(%r"'gems/json-[^/]+/lib")
-        script_contents.should match(%r"'gems/jruby-openssl-[^/]+/lib")
+        expect(script_contents).to match(%r"'gems/json-[^/]+/lib")
+        expect(script_contents).to match(%r"'gems/jruby-openssl-[^/]+/lib")
       end
     end
 
     it 'includes jruby-complete.jar' do
-      jar_entries.should include("lib/jruby-complete-#{JRUBY_VERSION}.jar")
+      expect(jar_entries).to include("lib/jruby-complete-#{JRUBY_VERSION}.jar")
     end
 
     it 'includes extra JAR dependencies' do
-      jar_entries.should include('lib/test_project_ext.jar')
+      expect(jar_entries).to include('lib/test_project_ext.jar')
     end
 
     it 'includes the Rubydoop runner and support classes' do
-      jar_entries.should include('rubydoop/RubydoopJobRunner.class')
-      jar_entries.should include('rubydoop/MapperProxy.class')
-      jar_entries.should include('rubydoop/ReducerProxy.class')
-      jar_entries.should include('rubydoop/CombinerProxy.class')
-      jar_entries.should include('rubydoop/InstanceContainer.class')
+      expect(jar_entries).to include('rubydoop/RubydoopJobRunner.class')
+      expect(jar_entries).to include('rubydoop/MapperProxy.class')
+      expect(jar_entries).to include('rubydoop/ReducerProxy.class')
+      expect(jar_entries).to include('rubydoop/CombinerProxy.class')
+      expect(jar_entries).to include('rubydoop/InstanceContainer.class')
     end
 
     it 'has the RubydoopJobRunner as its main class' do
-      jar.manifest.main_attributes.get(Java::JavaUtilJar::Attributes::Name::MAIN_CLASS).should == 'rubydoop.RubydoopJobRunner'
+      expect(jar.manifest.main_attributes.get(Java::JavaUtilJar::Attributes::Name::MAIN_CLASS)).to eq 'rubydoop.RubydoopJobRunner'
     end
   end
 
@@ -117,21 +117,21 @@ describe 'Packaging and running a project' do
       end
 
       it 'runs the mapper and reducer and writes the output in the specified directory' do
-        words['anything'].should == 21
+        expect(words['anything']).to eq 21
       end
 
       it 'runs the combiner' do
-        log.should match(/Combine input records=[^0]/)
-        words['alice'].should == 385 * 2
+        expect(log).to match(/Combine input records=[^0]/)
+        expect(words['alice']).to eq 385 * 2
       end
 
       %w(mapper reducer combiner).each do |type|
         it "runs the #{type} setup method" do
-          log.should match(/#{type.upcase}_SETUP_COUNT=1$/)
+          expect(log).to match(/#{type.upcase}_SETUP_COUNT=1$/)
         end
 
         it "runs the #{type} cleanup method" do
-          log.should match(/#{type.upcase}_CLEANUP_COUNT=1$/)
+          expect(log).to match(/#{type.upcase}_CLEANUP_COUNT=1$/)
         end
       end
     end
@@ -142,8 +142,8 @@ describe 'Packaging and running a project' do
       end
 
       it 'runs the mapper and reducer with secondary sorting through the use of a custom partitioner and grouping comparator' do
-        uniques['a'].should == 185
-        uniques['e'].should == 128
+        expect(uniques['a']).to eq 185
+        expect(uniques['e']).to eq 128
       end
     end
   end
